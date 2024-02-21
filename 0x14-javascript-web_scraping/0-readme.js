a8514177c050d4d8049a0da7102ba415065d9418
uluchukwu <urealsean12@gmail.com>
2024-02-20 18:24:49 -0800

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node read-file.js <file-path>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
-    console.error(`Error reading file: ${err.message}`);
+    console.log(err);
   } else {
-    console.log('File content:');
     console.log(data);
   }
 });
