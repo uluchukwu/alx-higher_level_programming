f103169c0a92a106406ed1ed4573b09168b8b6df
uluchukwu <urealsean12@gmail.com>
2024-02-20 18:22:40 -0800

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
-
-/*
- * A script that reads and prints the content of a file.
- * The first argument is the file path
- * The content of the file must be read in utf-8
- * If an error occurred during the reading, print the error object
-*/
-
 const fs = require('fs');
 
 if (process.argv.length !== 3) {
